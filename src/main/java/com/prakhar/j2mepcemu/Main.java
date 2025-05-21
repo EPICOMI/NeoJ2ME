5d15718ff1c46fbd188e5aa80aea508ae5ea86d9
Prakhar V. <prakharverma2006@gmail.com>
2025-05-22 00:29:58 +0100

    editing a bunch of files to add the freej2me-plus fork to my project

---
+++ src/main/java/com/prakhar/j2mepcemu/Main.java
@@ -0,0 +1,11 @@
+package com.prakhar.j2mepcemu;
+
+import org.recompile.freej2me.FreeJ2ME;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Core integrated!");
+        FreeJ2ME emulator = new FreeJ2ME(); // Assuming default constructor exists
+        // emulator.start(); // You might call a method here later
+    }
+}
